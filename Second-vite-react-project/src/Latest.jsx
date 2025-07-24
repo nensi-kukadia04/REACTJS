59b4f1f2061fa8dffb1da10d85c6b342c989d3a3
nensi-kukadia04 <kukadiyanensi838@gmail.com>
2025-07-24 18:55:40 +0530

    24-july-lecture

---
+++ Second-vite-react-project/src/Latest.jsx
@@ -0,0 +1,11 @@
+function Latest(){
+    return(
+        <>
+            <h1>Hello second vite react project</h1>
+            <p>This is the latest react project.</p>
+        </>
+    )
+        
+}
+
+export default Latest
